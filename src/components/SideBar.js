8d9d01d6c9a032db02a300f996b8a1549fdb347b
Makenzo04 <madiyar.talgatuly@inbox.ru>
2025-06-03 16:55:05 +0500

    fix

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -70,21 +70,6 @@ function SideBar({ serverResponse }) {
                       </p>
                       <p>
                         <strong>Description:</strong>
-                        <TypeAnimation
-                          sequence={[
-                            // Same substring at the start will only be typed out once, initially
-                            'We produce food for Mice',
-                            1000, // wait 1s before replacing "Mice" with "Hamsters"
-                            'We produce food for Hamsters',
-                            1000,
-                            'We produce food for Guinea Pigs',
-                            1000,
-                            'We produce food for Chinchillas',
-                            1000,
-                          ]}
-                          wrapper="span"
-                          speed={50}
-                        />
                         <span>{desc.description} </span>
                       </p>
                     </div>
